57462ee446b8d0de1b2c4b4dd155c82393a4a65a
maldad <canongoo72@gmail.com>
2019-05-21 16:26:08 -0500

    using AJAX to display the cart

--- agile-development-rails5/depot/app/controllers/line_items_controller.rb
+++ agile-development-rails5/depot/app/controllers/line_items_controller.rb
@@ -31,7 +31,10 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart}
+        format.html { redirect_to store_index_url }
+        format.js   { @current_item = @line_item }
+        #cuando se genera una peticion JS, se asigna una variable de instancia indicando
+        #el último item con el cual se ha interactuado
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
@@ -59,7 +62,7 @@ class LineItemsController < ApplicationController
   def destroy
     @line_item.destroy
     respond_to do |format|
-      format.html { redirect_to @line_item.cart, notice: 'Line item was successfully destroyed.' }
+      format.html { redirect_to store_index_url, notice: 'Line item was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
