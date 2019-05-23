9498a07aaf09afce01042a2c2cd7e707c0b5332c
maldad <canongoo72@gmail.com>
2019-05-23 17:54:13 -0500

    adding a drop down button to change the language

--- agile-development-rails5/depot/config/routes.rb
+++ agile-development-rails5/depot/config/routes.rb
@@ -7,14 +7,18 @@ Rails.application.routes.draw do
   end
 
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
-  get 'store/index'
-
   resources :products do
     get :who_bought, on: :member
   end
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
+  end
+
+  get 'store/index'
+
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
